3bfe5b5fc2bf85e74ce5e8da2cc134bb46b6d15e
TharunMedishetty <115418770+TharunMedishetty@users.noreply.github.com>
2022-10-10 23:06:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
  
 # loading the trained model
-pickle_in = open('Regression_Model.pickle', 'rb') 
+pickle_in = open('Regression_Model.pkl', 'rb') 
 Regression_Model = pickle.load(pickle_in)
  
 @st.cache()
